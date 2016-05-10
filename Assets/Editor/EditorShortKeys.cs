0cf2ca6a4d8bae171f8d59b40437250978a146cc
Diego Mazala <diegomazala@gmail.com>
2016-05-10 11:22:07 -0300

    Script to set hotkeys into editor

---
+++ Assets/Editor/EditorShortKeys.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+public class EditorShortKeys : ScriptableObject
+{
+    [MenuItem("HotKey/Run _F5")]
+    static void PlayGame()
+    {
+        EditorApplication.ExecuteMenuItem("Edit/Play");
+    }
+
+    [MenuItem("HotKey/Run _F9")]
+    static void PauseGame()
+    {
+        EditorApplication.ExecuteMenuItem("Edit/Pause");
+    }
+
+    [MenuItem("HotKey/Run _F10")]
+    static void StepGame()
+    {
+        EditorApplication.ExecuteMenuItem("Edit/Step");
+    }
+}
